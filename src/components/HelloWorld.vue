fdbdfb0254e5a5596cb0c4e53bb59064b8e9eba4
Ash Srinivas <asriniv4@its.jnj.com>
2023-01-11 23:59:32 -0800

    Added basic three.js poc to the commmon app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,53 @@
+<script setup>
+import { onMounted } from 'vue';
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer
+} from 'three';
+
+onMounted(() => {
+  // Get div for renderer
+  const rendererDiv = document.getElementById('rendererDiv')
+
+  const scene = new Scene();
+  console.log(scene);
+  const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  console.log(camera);
+
+  const renderer = new WebGLRenderer();
+  renderer.setSize( 800,  600);
+  rendererDiv.appendChild(renderer.domElement);
+
+  const geometry = new BoxGeometry( 1, 1, 1 );
+  const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+
+  function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+  }
+  animate();
+});
+
+
+</script>
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div id="rendererDiv">
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#rendererDiv {
+  border: 1px solid black;
 }
 </style>
